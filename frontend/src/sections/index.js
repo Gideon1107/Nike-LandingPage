0d20e7b9cb17f06a8f69045180a9b35fa6a6b253
Gideon1107 <ayenigideon1107@gmail.com>
2024-10-29 22:43:10 +0000

    initial commit

---
+++ frontend/src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from "./Hero";
+import PopularProducts from "./PopularProducts";
+import SpecialOffer from "./SpecialOffer";
+import Subscribe from "./Subscribe";
+import Services from "./Services";
+import Footer from "./Footer";
+import CustomerReviews from "./CustomerReviews";
+import SuperQuality from "./SuperQuality";
+
+export {
+    Hero,
+    PopularProducts,
+    SuperQuality,
+    Services,
+    SpecialOffer,
+    CustomerReviews,
+    Subscribe,
+    Footer
+}
